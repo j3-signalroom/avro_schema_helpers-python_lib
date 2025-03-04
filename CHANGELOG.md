e59b4c1f286fb1340b18d30aa7a36158123a5ba7
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2025-03-03 21:36:07 -0500

    Resolved #41.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to this project will be documented in this file.
 
 The format is base on [Keep a Changelog](https://keepachangelog.com/en/1.1.0/), and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [0.04.00.000] - 2025-03-03
+### Added
+- Issue [#41](https://github.com/j3-signalroom/ccaf-avro_schema_helpers-python_lib/issues/41)
+
 ## [0.03.02.000] - 2025-03-03
 ### Changed
 - Issue [#30](https://github.com/j3-signalroom/ccaf-avro_schema_helpers-python_lib/issues/30)
--- src/ccaf_avro_schema_helpers_python_lib/utility.py
+++ src/ccaf_avro_schema_helpers_python_lib/utility.py
@@ -22,3 +22,29 @@ def to_snake_case(name: str) -> str:
     """Convert a string (e.g. CamelCase or mixedCase) to snake_case."""
     # Insert an underscore before each capital letter (that isn't at start) and lowercase everything.
     return re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
+
+
+def convert_pyflink_to_avro_data_type(data_type) -> str:
+    """Convert PyFlink data type to Avro data type."""
+    if str(data_type).lower().startswith("double"):
+        return "double"
+    elif str(data_type).lower().startswith("varchar"):
+        return "string"
+    elif str(data_type).lower().startswith("row"):
+        return "record"
+    elif str(data_type).lower().startswith("array"):
+        return "array"
+    elif str(data_type).lower().startswith("boolean"):
+        return "boolean"
+    elif str(data_type).lower().startswith("tinyint"):
+        return "int"
+    elif str(data_type).lower().startswith("smallint"):
+        return "int"
+    elif str(data_type).lower().startswith("int"):
+        return "int"
+    elif str(data_type).lower().startswith("float"):
+        return "float"
+    elif str(data_type).lower().startswith("bigint"):
+        return "long"
+    else:
+        return "string"
